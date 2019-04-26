7900126d928a7c8d7c0efe9b4748269bdf5d8af8
Dan Shrader <dan@shraderland.com>
2019-04-26 16:56:30 +0000

    Updated to the build YAML and handler

--- test/handler.py
+++ test/handler.py
@@ -1,9 +1,12 @@
+import datetime
+
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-
+    now = datetime.datetime.now()
     print("hello there 2")
-
-    return req
+    print(str(req))
+    return  str(now)
